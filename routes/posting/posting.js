e9f15fc661832652dd7982eea4f541712c2fd7ae
Matt Swart <matt@Matts-MacBook-Pro.local>
2016-07-07 08:00:26 +1000

    enabled postings

--- routes/posting/posting.js
+++ routes/posting/posting.js
@@ -5,24 +5,36 @@ var router = express.Router();
 var postingDataAcccessService = new postingDataAccess.PostingDataAccess();
 router
     .get('/', function (req, res, next) {
+    /** Not secure at all, but great for local usage only */
+    res.header("Access-Control-Allow-Origin", "*");
+    /** -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- */
     postingDataAcccessService.init();
     postingDataAcccessService.find(function (err, postings) {
         res.status(200).send(postings);
     });
 })
     .get('/:id', function (req, res, next) {
+    /** Not secure at all, but great for local usage only */
+    res.header("Access-Control-Allow-Origin", "*");
+    /** -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- */
     postingDataAcccessService.init();
     postingDataAcccessService.findById(req.params.id, function (err, posting) {
         res.status(200).send(posting);
     });
 })
     .put('/:id', function (req, res, next) {
+    /** Not secure at all, but great for local usage only */
+    res.header("Access-Control-Allow-Origin", "*");
+    /** -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- */
     postingDataAcccessService.init();
     postingDataAcccessService.update(req.params.id, req.body, function (err, posting) {
         res.status(200).send(posting);
     });
 })
-    .put('/', function (req, res, next) {
+    .post('/', function (req, res, next) {
+    /** Not secure at all, but great for local usage only */
+    res.header("Access-Control-Allow-Origin", "*");
+    /** -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- */
     postingDataAcccessService.init();
     postingDataAcccessService.save(req.body, function (err, posting) {
         if (err === null) {
@@ -33,10 +45,25 @@ router
         }
     });
 })
+    .options('/', function (req, res, next) {
+    /** Not secure at all, but great for local usage only */
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT");
+    res.header("Access-Control-Allow-Headers", "Origin,Content-Type,Authorization,Accept");
+    res.header("Content-Type", "application/json");
+    /** -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- */
+    res.status(200).send("OK");
+})
     .post('/ping', function (req, res, next) {
+    /** Not secure at all, but great for local usage only */
+    res.header("Access-Control-Allow-Origin", "*");
+    /** -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- */
     res.status(200).send("[" + Date.now() + "] pong");
 })
     .post('/longprocess', function (req, res, next) {
+    /** Not secure at all, but great for local usage only */
+    res.header("Access-Control-Allow-Origin", "*");
+    /** -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- */
     res.status(200).send("[" + Date.now() + "] Not implemented yet");
 });
 module.exports = router;
--- routes/posting/posting.ts
+++ routes/posting/posting.ts
@@ -7,7 +7,9 @@ let postingDataAcccessService = new postingDataAccess.PostingDataAccess();
 
 router
     .get('/', function (req, res, next) {
-
+        /** Not secure at all, but great for local usage only */
+        res.header("Access-Control-Allow-Origin", "*");
+        /** -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- */
         postingDataAcccessService.init();
         postingDataAcccessService.find(function (err, postings) {
             res.status(200).send(postings);
@@ -15,7 +17,9 @@ router
 
     })
     .get('/:id', function (req, res, next) {
-
+        /** Not secure at all, but great for local usage only */
+        res.header("Access-Control-Allow-Origin", "*");
+        /** -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- */
         postingDataAcccessService.init();
         postingDataAcccessService.findById(req.params.id, function (err, posting) {
             res.status(200).send(posting);
@@ -23,26 +27,48 @@ router
 
     })
     .put('/:id', function (req, res, next) {
+        /** Not secure at all, but great for local usage only */
+        res.header("Access-Control-Allow-Origin", "*");
+        /** -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- */
         postingDataAcccessService.init();
         postingDataAcccessService.update(req.params.id, req.body, function (err, posting) {
             res.status(200).send(posting);
         });
     })
-    .put('/', function (req, res, next) {
+    .post('/', function (req, res, next) {
+        /** Not secure at all, but great for local usage only */
+        res.header("Access-Control-Allow-Origin", "*");
+        /** -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- */
         postingDataAcccessService.init();
         postingDataAcccessService.save(req.body, function (err, posting) {
-            if (err === null){
-            res.status(201).send(posting);
+            if (err === null) {
+                res.status(201).send(posting);
             }
             else {
                 res.status(500).send(err.message);
             }
         });
     })
+    .options('/', function (req, res, next) {
+        /** Not secure at all, but great for local usage only */
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Methods", "GET, POST, PUT");
+        res.header("Access-Control-Allow-Headers", "Origin,Content-Type,Authorization,Accept");
+        res.header("Content-Type", "application/json");
+        /** -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- */
+
+        res.status(200).send("OK");
+    })
     .post('/ping', function (req, res, next) {
+        /** Not secure at all, but great for local usage only */
+        res.header("Access-Control-Allow-Origin", "*");
+        /** -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- */
         res.status(200).send("[" + Date.now() + "] pong");
     })
     .post('/longprocess', function (req, res, next) {
+        /** Not secure at all, but great for local usage only */
+        res.header("Access-Control-Allow-Origin", "*");
+        /** -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- */
         res.status(200).send("[" + Date.now() + "] Not implemented yet");
     });
 
