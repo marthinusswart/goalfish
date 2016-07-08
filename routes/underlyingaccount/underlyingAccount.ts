0b5b2188428f3f687f3978720666d7f21d0c9f54
Matt Swart <matt@Matts-MacBook-Pro.local>
2016-07-08 18:21:39 +1000

    added options to REST service

--- routes/underlyingaccount/underlyingAccount.ts
+++ routes/underlyingaccount/underlyingAccount.ts
@@ -58,6 +58,16 @@ router
         /** -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- */
         res.status(200).send("OK");
     })
+    .options('/:id', function (req, res, next) {
+        /** Not secure at all, but great for local usage only */
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Methods", "GET, POST, PUT");
+        res.header("Access-Control-Allow-Headers", "Origin,Content-Type,Authorization,Accept");
+        res.header("Content-Type", "application/json");
+        /** -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- */
+
+        res.status(200).send("OK");
+    })
     .post('/ping', function (req, res, next) {
         /** Not secure at all, but great for local usage only */
         res.header("Access-Control-Allow-Origin", "*");
