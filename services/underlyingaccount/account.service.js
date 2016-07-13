f00bc4eb88c5e401b26b7e4be3963617d82aab71
Matt Swart <matt@Matts-MacBook-Pro.local>
2016-07-13 16:11:18 +1000

    added the reconciliation engine and provess

---
+++ services/underlyingaccount/account.service.js
@@ -0,0 +1,57 @@
+"use strict";
+var postingServiceLib = require('../../services/posting/posting.service');
+var postingDataAccessLib = require('../../dataaccess/posting/postingDataAccess');
+var accountControllerLib = require('../../controllers/underlyingaccount/underlyingAccountController');
+var accountDataAccessLib = require('../../dataaccess/underlyingaccount/underlyingAccountDataAccess');
+var async = require('async');
+var UnderlyingAccountService = (function () {
+    function UnderlyingAccountService() {
+    }
+    UnderlyingAccountService.prototype.reconcileAccounts = function (callback) {
+        var self = this;
+        this.postingService = new postingServiceLib.PostingService();
+        this.underlyingAccountController = new accountControllerLib.UnderlyingAccountController();
+        this.underlyingAccountDataAccess = new accountDataAccessLib.UnderlyingAccountDataAccess();
+        this.postingDataAccess = new postingDataAccessLib.PostingDataAccess();
+        this.underlyingAccountDataAccess.init();
+        this.postingDataAccess.init();
+        this.underlyingAccountDataAccess.find(findCallback);
+        function findCallback(err, accounts) {
+            if (err === null) {
+                var count_1 = 0;
+                async.whilst(function () { return count_1 < accounts.length; }, function (callbackWhilst) {
+                    var account = accounts[count_1];
+                    count_1++;
+                    account.isReconciled = false;
+                    var filter = { accountNumber: account.id };
+                    self.postingDataAccess.findByField(filter, findPostingCallback);
+                    function findPostingCallback(err, postings) {
+                        if (err === null) {
+                            var balance_1 = 0;
+                            postings.forEach(function (posting) {
+                                balance_1 += posting.amount;
+                            });
+                            account.calculatedBalance = balance_1;
+                            if (account.balance === account.calculatedBalance) {
+                                account.isReconciled = true;
+                            }
+                        }
+                        else {
+                            console.log("Error in finding postings");
+                        }
+                        callbackWhilst();
+                    }
+                }, function (err) {
+                    callback(err, accounts);
+                });
+            }
+            else {
+                console.log("Failed to load accounts");
+                callback(err, []);
+            }
+        }
+    };
+    return UnderlyingAccountService;
+}());
+exports.UnderlyingAccountService = UnderlyingAccountService;
+//# sourceMappingURL=account.service.js.map
---
+++ services/underlyingaccount/account.service.ts
@@ -0,0 +1,72 @@
+
+import postingServiceLib = require('../../services/posting/posting.service');
+import postingDataAccessLib = require('../../dataaccess/posting/postingDataAccess');
+import postingLib = require('../../models/posting/posting');
+import accountLib = require('../../models/underlyingaccount/underlyingaccount');
+import accountControllerLib = require('../../controllers/underlyingaccount/underlyingAccountController');
+import accountDataAccessLib = require('../../dataaccess/underlyingaccount/underlyingAccountDataAccess');
+
+import async = require('async');
+
+export class UnderlyingAccountService {
+    postingService: postingServiceLib.PostingService;
+    underlyingAccount: accountLib.UnderlyingAccount;
+    underlyingAccountController: accountControllerLib.UnderlyingAccountController;
+    underlyingAccountDataAccess: accountDataAccessLib.UnderlyingAccountDataAccess;
+    postingDataAccess: postingDataAccessLib.PostingDataAccess;
+
+    reconcileAccounts(callback) {
+        let self = this;
+        this.postingService = new postingServiceLib.PostingService();
+        this.underlyingAccountController = new accountControllerLib.UnderlyingAccountController();
+        this.underlyingAccountDataAccess = new accountDataAccessLib.UnderlyingAccountDataAccess();
+        this.postingDataAccess = new postingDataAccessLib.PostingDataAccess();
+        this.underlyingAccountDataAccess.init();
+        this.postingDataAccess.init();
+
+        this.underlyingAccountDataAccess.find(findCallback);
+
+        function findCallback(err, accounts) {
+            if (err === null) {
+                let count = 0;
+
+                async.whilst(() => { return count < accounts.length; },
+                    (callbackWhilst) => {
+                        let account: accountLib.UnderlyingAccount = accounts[count];
+                        count++;
+                        account.isReconciled = false;
+                        let filter = { accountNumber: account.id };
+                        self.postingDataAccess.findByField(filter, findPostingCallback);
+
+                        function findPostingCallback(err, postings) {
+                            if (err === null) {
+                                let balance: number = 0;
+                                postings.forEach((posting: postingLib.Posting) => {                                    
+                                    balance += posting.amount;
+                                });
+                                account.calculatedBalance = balance;
+                                if (account.balance === account.calculatedBalance) {
+                                    account.isReconciled = true;                                    
+                                } 
+                            } else {
+                                console.log("Error in finding postings");
+                            }
+
+                            callbackWhilst();
+                        }
+                    },
+                    (err) => {
+                        callback(err, accounts);
+                    });
+
+            } else {
+                console.log("Failed to load accounts");
+                callback(err, []);
+            }
+
+
+        }
+
+    }
+
+}
