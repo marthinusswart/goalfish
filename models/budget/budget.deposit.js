e31e0e1aef3cd507c6e9d57ab7b3dbed69ad0274
Matt Swart <matt@Matts-MacBook-Pro.local>
2016-07-19 21:21:39 +1000

    added

---
+++ models/budget/budget.deposit.js
@@ -0,0 +1,8 @@
+"use strict";
+var BudgetDeposit = (function () {
+    function BudgetDeposit() {
+    }
+    return BudgetDeposit;
+}());
+exports.BudgetDeposit = BudgetDeposit;
+//# sourceMappingURL=budget.deposit.js.map
---
+++ models/budget/budget.deposit.ts
@@ -0,0 +1,9 @@
+
+export class BudgetDeposit {
+    budgetId: string;
+    fromAccountId: string;
+    toAccountId: string;
+    description: string;
+    amount: number;
+    depositDate: Date;
+}
