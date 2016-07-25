ac30843849f21c483d1d2f15c3423ebcf06152d7
Matt Swart <matt@Matts-MacBook-Pro.local>
2016-07-25 16:24:26 +1000

    added withdrawals

---
+++ models/budget/budget.withdrawal.js
@@ -0,0 +1,8 @@
+"use strict";
+var BudgetWithdrawal = (function () {
+    function BudgetWithdrawal() {
+    }
+    return BudgetWithdrawal;
+}());
+exports.BudgetWithdrawal = BudgetWithdrawal;
+//# sourceMappingURL=budget.withdrawal.js.map
---
+++ models/budget/budget.withdrawal.ts
@@ -0,0 +1,8 @@
+
+export class BudgetWithdrawal {
+    budgetId: string;
+    fromAccountId: string;
+    description: string;
+    amount: number;
+    withdrawalDate: Date;
+}
