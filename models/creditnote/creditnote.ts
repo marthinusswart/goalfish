6fd14a21c8502fdf2427840aca2b908a85d8d68c
Matt Swart <matt@Matts-MacBook-Pro.local>
2016-07-26 06:42:41 +1000

    renamed controller

---
+++ models/creditnote/creditnote.ts
@@ -0,0 +1,9 @@
+export class CreditNote {
+    externalRef: string;
+    id: string;
+    name: string;
+    description: string;
+    balance: number;
+    underlyingAccount: string;
+    memberId: string;
+}
